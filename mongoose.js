e8749cb91e5191861f973b54d9f02bdd1c5b6435
Jacob Peterson <petersonjacobw@gmail.com>
2021-03-16 11:11:48 -0700

    Added individual review schema to populate reviews array in product schema

--- mongoose.js
+++ mongoose.js
@@ -4,36 +4,35 @@ mongoose.connect('mongodb://localhost/reviews', {useNewUrlParser: true, useCreat
 let productReviewsSchema = mongoose.Schema({
   product: {type: Number, unique: true},
   count: Number,
-  results: [
-    {
-      review_id: {type: Number, unique: true},
-      rating: { type: Number, min: 0, max: 5 },
-      summary: { type: String, required: true, trim: true, maxLength: 100 },
-      recommend: Boolean,
-      body: { type: String, required: true, trim: true, },
-      response: String,
-      date: { type: Date, default: Date.now },
-      reviewer_name: { type: String, required: true, trim: true, maxLength: 60 },
-      email: { type: String, required: true, trim: true, maxLength: 60 },
-      helpfulness: Number,
-      reported: Boolean,
-      photos: [{
-          id: {type: Number, unique: true},
-          url: { type: String, required: true, trim: true, },
-        },
-      ],
-    },
-  ]
-});
-
-let ProductReviews = mongoose.model('ProductReviews', productReviewsSchema);
-
-let characteristicsSchema = mongoose.Schema({
-  characteristic_id: {
+  reviews: [{ type : ObjectId, ref: 'Review' }]
+  characteristics: {
+    characteristic_id: Number
     characteristic: String,
     total_value: Number,
     total_reviews: Number,
   }
 });
 
-let Characteristics = mongoose.model('characteristicsDb', characteristicsSchema);
+let ProductReviews = mongoose.model('ProductReviews', productReviewsSchema);
+
+
+let reviewSchema = mongoose.Schema({
+  review_id: {type: Number, unique: true},
+  rating: { type: Number, min: 0, max: 5 },
+  summary: { type: String, required: true, trim: true, maxLength: 100 },
+  recommend: Boolean,
+  body: { type: String, required: true, trim: true, },
+  response: String,
+  date: { type: Date, default: Date.now },
+  reviewer_name: { type: String, required: true, trim: true, maxLength: 60 },
+  email: { type: String, required: true, trim: true, maxLength: 60 },
+  helpfulness: Number,
+  reported: Boolean,
+  photos: [{
+      id: {type: Number, unique: true},
+      url: { type: String, required: true, trim: true, },
+    },
+  ],
+});
+
+let Review = mongoose.model('Review', ReviewSchema);
