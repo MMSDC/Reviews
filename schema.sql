50dc5d7a9f0329bfd302e7561d722aaf07203b25
Jacob Peterson <petersonjacobw@gmail.com>
2021-03-17 22:57:56 -0700

    Reformatted postgres structure

--- schema.sql
+++ schema.sql
@@ -2,37 +2,31 @@
 
 -- CREATE DATABASE reviews;
 
--- USE reviews;
-
 CREATE TABLE reviews(
-   review_id SERIAL PRIMARY KEY,
+   id SERIAL PRIMARY KEY,
+   product_id INTEGER NOT NULL,
    rating SMALLINT,
+   date VARCHAR(10) NOT NULL,
    summary VARCHAR(100) NOT NULL,
    body TEXT NOT NULL,
-   reviewer_name VARCHAR(60) NOT NULL,
-   email VARCHAR(60) NOT NULL,
-   response TEXT,
    recommend BOOLEAN,
-   date TIMESTAMP,
-   helpfulness SMALLINT,
-   reported BOOLEAN
+   reported BOOLEAN,
+   reviewer_name VARCHAR(60) NOT NULL,
+   reviewer_email VARCHAR(60) NOT NULL,
+   response TEXT,
+   helpfulness SMALLINT
 );
 
 CREATE TABLE images(
-  review_id INTEGER NOT NULL REFERENCES reviews,
   id SERIAL,
+  review_id INTEGER NOT NULL REFERENCES reviews,
   url TEXT NOT NULL
 );
 
-CREATE TABLE product_reviews(
-  product_id INTEGER NOT NULL,
-  review_id INTEGER NOT NULL REFERENCES reviews
-);
-
 CREATE TABLE characteristics(
+  id SERIAL,
   product_id INTEGER NOT NULL,
-  characteristic_id SERIAL,
-  characteristic VARCHAR(20) NOT NULL,
+  name VARCHAR(20) NOT NULL,
   total_from_reviews INTEGER,
   total_reviews INTEGER
 );
